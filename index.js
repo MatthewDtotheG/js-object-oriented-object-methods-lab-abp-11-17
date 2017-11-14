f85c88f2444437631cbdb5c9dce0be0c244ecaaf
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-14 01:38:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function BoardMember(name, homeState, training) {
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
-}
-
 BoardMember.prototype.veto = function() {
   return 'No, I must disagree';
 };
@@ -21,10 +15,8 @@ BoardMember.prototype.releasePressStatement = function() {
 };
 
 BoardMember.prototype.sayHi = function() {
-<<<<<<< HEAD
   return `Hi, my name is ${this.name}. I am from ${this
     .homeState}, and I was trained in ${this.training}.`;
-=======
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
->>>>>>> 8f819fd567fc235394a22192a01592fde9a49e19
+
 };
