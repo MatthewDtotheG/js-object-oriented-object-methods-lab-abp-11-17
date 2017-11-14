2a07f0a45de14374f2e36b40489680c87b58944b
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-14 01:37:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ BoardMember.prototype.releasePressStatement = function() {
 };
 
 BoardMember.prototype.sayHi = function() {
+<<<<<<< HEAD
   return `Hi, my name is ${this.name}. I am from ${this
     .homeState}, and I was trained in ${this.training}.`;
+=======
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+>>>>>>> 8f819fd567fc235394a22192a01592fde9a49e19
 };
